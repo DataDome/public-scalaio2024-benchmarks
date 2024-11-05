1877e5079fff2a5078f9e8a38754741dc057bc42
Gaël Renoux <gael.renoux@datadome.co>
2024-11-05 14:52:18 +0100

    Script to benchmark for the slides

---
+++ slides-benchmark.sh
@@ -0,0 +1,15 @@
+IT=20
+WIT=10
+
+sbt clean compile
+
+sbt "Jmh/run -i $IT -wi $WIT -f1 -t1 .*chapter2.IterationBenchmark_Large.compute[01].*" | tee -a chapter2-iteration.report.txt
+sbt "Jmh/run -i $IT -wi $WIT -f1 -t1 .*chapter3.CollectionStringIterationBenchmark_(Small|Large)" | tee -a chapter3-iteration.report.txt
+sbt "Jmh/run -i $IT -wi $WIT -f1 -t1 .*chapter3.AppendBenchmark_String_Large" | tee -a chapter3-append.report.txt
+sbt "Jmh/run -i $IT -wi $WIT -f1 -t1 .*chapter3.SetBenchmark_.*Invoice_HalfHit_Large.*" | tee -a chapter3-sets.report.txt
+sbt "Jmh/run -i $IT -wi $WIT -f1 -t1 .*chapter3.MapBenchmark_Invoice_HalfHit_(Small|Large).*" | tee -a chapter3-maps.report.txt
+sbt "Jmh/run -i $IT -wi $WIT -f1 -t1 .*chapter4.StatsBenchmark_(Small|Large).foreach.*" | tee -a chapter4-stats.report.txt
+sbt "Jmh/run -i $IT -wi $WIT -f1 -t1 .*chapter4.GameBenchmark(Small|Large).*" | tee -a chapter4-game.report.txt
+
+
+
